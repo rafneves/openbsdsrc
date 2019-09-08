ff5aa3bac25141cb0616b96739bcbe9c2deec894
Rafael Neves <rafael@diskless.io>
2019-09-08 19:24:07 -0300

    Remove redundant unmount(2) calls and make return values more consistent
    Caller should umount(2) its mountpoints, so remove redundant umount(dst)
    from copy(). While here, make gettmpmnt() return values more consistent
    with other parts of the code, and adjust the checks according.

--- sbin/newfs/newfs.c
+++ sbin/newfs/newfs.c
@@ -517,7 +517,7 @@ havelabel:
 		char tmpnode[PATH_MAX];
 		int ret = 0;
 
-		if (pop != NULL && gettmpmnt(tmpnode, sizeof(tmpnode)) <= 0)
+		if (pop != NULL && gettmpmnt(tmpnode, sizeof(tmpnode)) != 0)
 			errx(1, "Cannot create tmp mountpoint for -P");
 		memset(&args, 0, sizeof(args));
 		args.base = membase;
@@ -759,7 +759,7 @@ isdir(const char *path)
 static int 
 copy(char *src, char *dst)
 {
-	int ret, dir, created = 0;
+	int ret, dir;
 	struct ufs_args mount_args;
 	char mountpoint[MNAMELEN];
 	char *const argv[] = { "pax", "-rw", "-pe", ".", dst, NULL } ;
@@ -770,8 +770,7 @@ copy(char *src, char *dst)
 	if (dir)
 		strlcpy(mountpoint, src, sizeof(mountpoint));
 	else {
-		created = gettmpmnt(mountpoint, sizeof(mountpoint));
-		if (created <= 0)
+		if (gettmpmnt(mountpoint, sizeof(mountpoint)) != 0)
 			return (-1);
 
 		memset(&mount_args, 0, sizeof(mount_args));
@@ -779,10 +778,8 @@ copy(char *src, char *dst)
 		ret = mount(MOUNT_FFS, mountpoint, MNT_RDONLY, &mount_args);
 		if (ret != 0) {
 			int saved_errno = errno;
-			if (created && rmdir(mountpoint) != 0)
+			if (rmdir(mountpoint) != 0)
 				warn("rmdir %s", mountpoint);
-			if (unmount(dst, 0) != 0)
-				warn("unmount %s", dst);
 			warnc(saved_errno, "mount %s %s", src, mountpoint);
 			return (-1);
 		}
@@ -790,7 +787,7 @@ copy(char *src, char *dst)
 	ret = do_exec(mountpoint, "/bin/pax", argv);
 	if (!dir && unmount(mountpoint, 0) != 0)
 		warn("unmount %s", mountpoint);
-	if (created && rmdir(mountpoint) != 0)
+	if (!dir && rmdir(mountpoint) != 0)
 		warn("rmdir %s", mountpoint);
 	if (ret != 0) {
 		if (unmount(dst, 0) != 0)
@@ -827,7 +824,7 @@ gettmpmnt(char *mountpoint, size_t len)
 			warnx("tmp mountpoint %s too long", mnt);
 			return (-1);
 		}
-		return (0);
+		return (-1);
 	}
 	n = strlcpy(mountpoint, tmp, len);
 	if (n >= len) {
@@ -845,7 +842,7 @@ gettmpmnt(char *mountpoint, size_t len)
 		warn("mkdtemp %s", mountpoint);
 		return (-1);
 	}
-	return (1);
+	return (0);
 }
 
 #endif /* MFS */
